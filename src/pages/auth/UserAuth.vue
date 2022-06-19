d6a84b2488d9ae1da41beac07e7f76e56f133409
Alex Mwangi <alex@stl-horizon.com>
2022-06-19 19:13:12 +0300

    Dispatched email/password to signup action once the validation passes when the mode is not login

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -54,7 +54,14 @@ export default {
                 return ;
             }
 
-            //send http request
+            if(this.mode === 'login'){
+                //...
+            }else{
+                this.$store.dispatch('signup',{
+                    email : this.email ,
+                    password : this.password
+                })
+            }
         },
 
         switchAuthMode(){
